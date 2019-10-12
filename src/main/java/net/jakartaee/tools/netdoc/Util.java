f8e4a15c57fe9ae1d11dddec812cef730d6736b8
DoctorJava <scott.forbes.in@gmail.com>
2019-10-12 14:13:03 -0500

    Cleaned up imports.

--- src/main/java/net/jakartaee/tools/netdoc/Util.java
+++ src/main/java/net/jakartaee/tools/netdoc/Util.java
@@ -11,7 +11,6 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
 
 public class Util {
